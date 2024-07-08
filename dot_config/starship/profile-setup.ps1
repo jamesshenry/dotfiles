9fe16030d4f7e77f95afbc967651dfbb0d311acd
James Henry <jhenry@vigosoftware.com>
2024-07-08 09:55:42 +0100

    Update .chezmoi.toml Add .chezmoiignore Add .config/starship/cache/.keep Add .config/starship/profile-setup.ps1 Add .config/starship/starship.toml

---
+++ dot_config/starship/profile-setup.ps1
@@ -0,0 +1,22 @@
+Write-Host "Initialising starship..." -ForegroundColor Magenta
+
+$env:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
+$env:STARSHIP_CACHE = "$HOME\.config\starship\cache"
+
+function Invoke-Starship-PreCommand {
+    $loc = $executionContext.SessionState.Path.CurrentLocation;
+    $prompt = "$([char]27)]9;12$([char]7)"
+    if ($loc.Provider.Name -eq "FileSystem") {
+        $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+    }
+    $host.ui.Write($prompt)
+}
+
+function Invoke-Starship-TransientFunction {
+    &starship module character
+}
+
+Invoke-Expression (&starship init powershell)
+
+Enable-TransientPrompt
+Write-Host "Initialised" -ForegroundColor Green
