5baa99652e60b2a22f1878594400bf9e44b8a28f
James Henry <jhenry@vigosoftware.com>
2024-10-28 11:01:35 +0000

    wip

--- dot_config/autohotkey/profile-setup.ps1
+++ dot_config/autohotkey/profile-setup.ps1
@@ -1,23 +1,23 @@
-Write-Host "Initialising autohotkey..." -ForegroundColor Magenta
+# Write-Host "Initialising autohotkey..." -ForegroundColor Magenta
 
-[FileInfo]$compiledScripts = . "$PSScriptRoot\compile-scripts.ps1"
+# [FileInfo]$compiledScripts = . "$PSScriptRoot\compile-scripts.ps1"
 
-$shellStartupPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\"
-$startupLinks = Get-ChildItem $shellStartupPath
-$exesPath = "$PSScriptRoot\exes\"
+# $shellStartupPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\"
+# $startupLinks = Get-ChildItem $shellStartupPath
+# $exesPath = "$PSScriptRoot\exes\"
 
-foreach ($exe in $compiledScripts) {
-    $newLink = [System.IO.FileInfo]::new("$shellStartupPath$($exe.Name)")
-    if (-not $startupLinks.Name.Contains($newLink.Name)) {
-        Write-Host "New script found: $($newLink.Name)"
-        New-Item -Path $newLink -ItemType SymbolicLink -Value $exe
-    }
+# foreach ($exe in $compiledScripts) {
+#     $newLink = [System.IO.FileInfo]::new("$shellStartupPath$($exe.Name)")
+#     if (-not $startupLinks.Name.Contains($newLink.Name)) {
+#         Write-Host "New script found: $($newLink.Name)"
+#         New-Item -Path $newLink -ItemType SymbolicLink -Value $exe
+#     }
 
-    $process = Get-Process "$($exe.BaseName)" -ErrorAction SilentlyContinue
-    if ($null -eq $process) {
-        Write-Host "Starting $($exe.BaseName) script" -ForegroundColor Yellow
-        Start-Process $newLink
-    }
-}
+#     $process = Get-Process "$($exe.BaseName)" -ErrorAction SilentlyContinue
+#     if ($null -eq $process) {
+#         Write-Host "Starting $($exe.BaseName) script" -ForegroundColor Yellow
+#         Start-Process $newLink
+#     }
+# }
 
-Write-Host "Initialised" -ForegroundColor Green
+# Write-Host "Initialised" -ForegroundColor Green
