a42a1571fdc7c324657a5125f941f11f07dce12f
henry-js <henry-js@outlook.com>
2025-01-03 03:34:52 +0000

    update

--- dot_config/autohotkey/profile-setup.ps1
+++ dot_config/autohotkey/profile-setup.ps1
@@ -1,23 +1,5 @@
-# Write-Host "Initialising autohotkey..." -ForegroundColor Magenta
+Write-Host "Initialising autohotkey..." -ForegroundColor Magenta
 
-# [FileInfo]$compiledScripts = . "$PSScriptRoot\compile-scripts.ps1"
+$env:Path = $env:Path + ";$PSScriptRoot\bin\v2" 
 
-# $shellStartupPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\"
-# $startupLinks = Get-ChildItem $shellStartupPath
-# $exesPath = "$PSScriptRoot\exes\"
-
-# foreach ($exe in $compiledScripts) {
-#     $newLink = [System.IO.FileInfo]::new("$shellStartupPath$($exe.Name)")
-#     if (-not $startupLinks.Name.Contains($newLink.Name)) {
-#         Write-Host "New script found: $($newLink.Name)"
-#         New-Item -Path $newLink -ItemType SymbolicLink -Value $exe
-#     }
-
-#     $process = Get-Process "$($exe.BaseName)" -ErrorAction SilentlyContinue
-#     if ($null -eq $process) {
-#         Write-Host "Starting $($exe.BaseName) script" -ForegroundColor Yellow
-#         Start-Process $newLink
-#     }
-# }
-
-# Write-Host "Initialised" -ForegroundColor Green
+Write-Host "Initialised" -ForegroundColor Green
--- dot_config/autohotkey/profile.ps1
+++
@@ -1,5 +0,0 @@
-Write-Host "Initialising autohotkey..." -ForegroundColor Magenta
-
-$env:Path = $env:Path + ";$PSScriptRoot\bin\v2" 
-
-Write-Host "Initialised" -ForegroundColor Green
