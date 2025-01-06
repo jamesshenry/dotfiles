52fafab08283571051a1ae4556c3afc27eeb4e4f
James Henry <jhenry@vigosoftware.com>
2025-01-06 17:36:26 +0000

    Remove .config/install-modules.ps1

--- dot_config/install-modules.ps1
+++
@@ -1,8 +0,0 @@
-
-$profileSetupList = Get-ChildItem "$PSScriptRoot\*\install.ps1" -Recurse -Depth 1
-
-foreach ($item in $profileSetupList) {
-    $moduleName = Split-Path (Split-Path $item) -Leaf
-    Write-Host "Installing $moduleName..." -ForegroundColor Cyan
-    . $item
-}
