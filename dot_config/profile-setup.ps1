dfbf50831390fc38f9d05be5437c008426363765
James Henry <James.Henry@kerridgecs.com>
2025-09-22 10:24:10 +0100

    remove profile-setup.ps1

--- dot_config/profile-setup.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host "Checking .config folder for profile-setup.ps1 scripts..."
-
-$profileSetupList = (Get-ChildItem "$PSScriptRoot\*\profile.ps1").FullName
-
-foreach ($item in $profileSetupList) {
-    . $item
-}
