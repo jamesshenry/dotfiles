c01ddb69d7304cde44bc9306311bb3cd525f104b
henry-js <79054685+henry-js@users.noreply.github.com>
2026-01-28 14:32:00 +0000

    Add .config/powershell/profile.ps1

---
+++ dot_config/powershell/profile.ps1
@@ -0,0 +1,4 @@
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+Invoke-Expression (& { (atuin init powershell | Out-String) })
+Invoke-Expression (& { (starship init powershell | Out-String) })
+
