d47313b6bab2ed8d39f47e92a05d28804ad08a89
James Henry <James.Henry@kerridgecs.com>
2026-01-29 10:23:05 +0000

    Update AppData/Roaming/helix/languages.toml Update .config/powershell/profile.ps1

--- dot_config/powershell/profile.ps1
+++ dot_config/powershell/profile.ps1
@@ -2,3 +2,13 @@ Invoke-Expression (& { (zoxide init powershell | Out-String) })
 Invoke-Expression (& { (atuin init powershell | Out-String) })
 Invoke-Expression (& { (starship init powershell | Out-String) })
 
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -Colors @{ InlinePrediction = '#8A0303' }
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+# Aliases
+New-Alias -Name grep -Value Select-String -Force
+
+# Env vars
+
+. "$PSScriptRoot\profile-setup-handler.ps1"
