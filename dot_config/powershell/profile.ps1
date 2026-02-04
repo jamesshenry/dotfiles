b87c48fd8bd289c84468c9019bfa6b4e3a36b9aa
jamesshenry <git@mail.jamesshenry.dev>
2026-02-04 13:55:55 +0000

    Update .config/powershell/profile.ps1

--- dot_config/powershell/profile.ps1
+++ dot_config/powershell/profile.ps1
@@ -1,7 +1,17 @@
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
+Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
 Invoke-Expression (& { (atuin init powershell | Out-String) })
 Invoke-Expression (& { (starship init powershell | Out-String) })
 
+function Invoke-Starship-PreCommand {
+    $loc = $executionContext.SessionState.Path.CurrentLocation;
+    $prompt = "$([char]27)]9;12$([char]7)"
+    if ($loc.Provider.Name -eq 'FileSystem') {
+        $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+    }
+    $host.ui.Write($prompt)
+}
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 # Set-PSReadLineOption -Colors @{ InlinePrediction = '#8A0303' }
 Set-PSReadLineOption -PredictionViewStyle InlineView
+
+Set-Alias -Name lg -Value lazygit
