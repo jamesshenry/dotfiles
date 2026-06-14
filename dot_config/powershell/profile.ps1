9eed7c482c3f3e2b67ca06c98c21d0f770071efb
henry-js <79054685+henry-js@users.noreply.github.com>
2026-06-14 12:13:27 +0100

    Update .config/powershell/profile.ps1

--- dot_config/powershell/profile.ps1
+++ dot_config/powershell/profile.ps1
@@ -1,13 +1,23 @@
+# Force UTF-8 for console output and piping
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+$OutputEncoding = [System.Text.Encoding]::UTF8
+
 Invoke-Expression (& { (atuin init powershell | Out-String) })
 Invoke-Expression (& { (starship init powershell | Out-String) })
 
-function Invoke-Starship-PreCommand {
-    $loc = $executionContext.SessionState.Path.CurrentLocation;
-    $prompt = "$([char]27)]9;12$([char]7)"
-    if ($loc.Provider.Name -eq 'FileSystem') {
-        $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
-    }
-    $host.ui.Write($prompt)
-}
+# function Invoke-Starship-PreCommand {
+#     $loc = $executionContext.SessionState.Path.CurrentLocation;
+#     $prompt = "$([char]27)]9;12$([char]7)"
+#     if ($loc.Provider.Name -eq 'FileSystem') {
+#         $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+#     }
+#     $host.ui.Write($prompt)
+# }
 Set-Alias -Name lg -Value lazygit
 Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
+
+
+# Add to $PROFILE
+function prompt {
+    prmt --code $LASTEXITCODE '{path:cyan:s} {git:purple:s:on :} {ok:green}{fail:red} '
+}
