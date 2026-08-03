ff465458b5d3e689a64e1c4d810a4255b729d5c9
jamesshenry <git@mail.jamesshenry.dev>
2026-08-03 13:14:07 +0100

    update various

--- dot_config/powershell/profile.ps1
+++ dot_config/powershell/profile.ps1
@@ -2,17 +2,12 @@
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 $OutputEncoding = [System.Text.Encoding]::UTF8
 
-Invoke-Expression (& { (atuin init powershell | Out-String) })
-Invoke-Expression (& { (starship init powershell | Out-String) })
+if (Get-Module -ListAvailable PSReadLine) {
+    Set-PSReadLineKeyHandler -Chord Ctrl+u -Function BackwardDeleteInput
+}
 
-# function Invoke-Starship-PreCommand {
-#     $loc = $executionContext.SessionState.Path.CurrentLocation;
-#     $prompt = "$([char]27)]9;12$([char]7)"
-#     if ($loc.Provider.Name -eq 'FileSystem') {
-#         $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
-#     }
-#     $host.ui.Write($prompt)
-# }
+Invoke-Expression (& { (atuin init powershell | Out-String) })
+
 Set-Alias -Name lg -Value lazygit
 Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
 
